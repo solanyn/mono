ca8ba6c9268195d3b9c49630d38476dd983b70c1
solanyn <14799876+solanyn@users.noreply.github.com>
2025-01-13 01:19:10 +1100

    feat(ears): add ears, jax model trainer module

---
+++ ears/README.md
@@ -0,0 +1,8 @@
+# goyangi.ears
+
+## Models
+
+- ResNet50 model ([He *et al.*, 2016])
+
+[He *et al.*, 2016]: https://arxiv.org/abs/1512.03385
+
---
+++ ears/main.py
@@ -0,0 +1,9 @@
+from resnet.model import Resnet
+
+
+def main():
+    print("Hello from goyangi.ears!")
+
+
+if __name__ == "__main__":
+    main()
