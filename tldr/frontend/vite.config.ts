09c95e45747ee1c74eca124886588f3a4cc51b8b
solanyn <14799876+solanyn@users.noreply.github.com>
2025-07-27 15:29:37 +1000

    fix(tldr): frontend build

--- tldr/frontend/vite.config.ts
+++ tldr/frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
+  base: "/",
+  plugins: [svgr(), react()],
+  test: {
+    globals: true,
+    css: true,
+    reporters: ["verbose"],
   },
-})
+});
+
